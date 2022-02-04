d43ff6b1cf2e2c2c748ee41217ca031a01f51b38
aytajramazanly <aytajshr@code.edu.az>
2022-02-04 04:40:31 +0400

    homework added

---
+++ Task-1/Task1/Task1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task1.DataAccessLayer;
+using Task1.ViewModels;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _dbContext.Products.Include(x => x.Category).ToList();
+            var categories = _dbContext.Categories.ToList();
+
+
+            return View(new HomeViewModel { 
+            Products=products,
+            Categories=categories
+            });
+        }
+    }
+}
---
+++ Task-1/Task1/Task1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task1.Models;
+
+namespace Task1.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
